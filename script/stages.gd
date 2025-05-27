475925cd0d71d0f63dabed294653a12e877f5b77
KazeShigami <elpresidentezerotwo02@gmail.com>
2025-05-27 12:08:33 +0800

    final final

--- script/stages.gd
+++ script/stages.gd
@@ -66,7 +66,7 @@ func _on_back_button_pressed() -> void:
 
 func _on_main_menu_button_pressed() -> void:
 	$".".visible = false
-	Globals.emit_signal("stages_pressed_mainmenu")
+	get_tree().change_scene_to_file("res://scene/tower_defense_main_menu.tscn")
 
 func _on_select_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/game.tscn")
